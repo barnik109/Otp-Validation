cbf59b7c847c3b8297423b4bc746808047017d7b
barnik109 <maitybarnik@gmail.com>
2023-03-28 06:36:38 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { BsFillShieldLockFill, BsTelephoneFill } from "react-icons/bs"
+import {CgSpinner} from 'react-icons/cg'
+import OtpInput from 'otp-input-react'
+import PhoneInput from "react-phone-input-2"
+import "react-phone-input-2/lib/style.css"
+import { auth } from "./firebase.config"
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth"
+import { toast, Toaster } from "react-hot-toast";
+import './css/main.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+  const [otp, setOtp] = useState("")
+  const [user,setUser]=useState(null)
+  const [ph,setPh]=useState("")
+  const [loading, setLoading] = useState(false)
+  const [showOtp, setShowOtp] = useState(false)
+
+  function OnCaptchVerify() {
+    if (!window.recaptchaVerifier) {
+      window.recaptchaVerifier = new RecaptchaVerifier("recaptcha-container", {
+        'size': 'invisible',
+        'callback': (response) => {
+          onSignup()
+        },
+        'expired-callback': () => {
+
+        }
+      }, auth);
+    }
+  }
+
+  
+  function onSignup() {
+    setLoading(true)
+    OnCaptchVerify()
+
+    const appVerifier = window.recaptchaVerifier
+
+    const formatPh = '+' + ph
+    signInWithPhoneNumber(auth, formatPh, appVerifier)
+      .then((confirmationResult) => {
+        window.confirmationResult = confirmationResult;
+        setLoading(false)
+        setShowOtp(true)
+        toast.success("OTP sended successfully!")
+      }).catch((error) => {
+        console.log(error)
+        setLoading(false)
+      });
+  }
+
+  function onOTPVerify() {
+    setLoading(true);
+    window.confirmationResult
+      .confirm(otp)
+      .then(async (res) => {
+        console.log(res);
+        setUser(res.user);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }
+
+
+
+  return (<section className='outer'>
+    <div>
+      <Toaster toastOptions={{duration:4000}}/>
+     <div id="recaptcha-container"></div>
+      {user ? (
+        <h2>
+          &#x1F44D; Login Success
+        </h2>
+      ) : (
+          <div className='heading'>
+            <h1 className='mainhd'>
+              Please validate <br /> your Phone 
+            </h1>
+
+            {showOtp ?
+              (<>
+                <div className='logo'>
+                  <BsFillShieldLockFill size={30} />
+                </div>
+                <label htmlFor="otp" className='phone'>
+                  Enter Your OTP
+                </label>
+                <OtpInput
+                  value={otp}
+                  onChange={setOtp}
+                  OTPLength={6}
+                  otpType="number"
+                  disabled={false}
+                  autoFocus
+                className='opt-container'></OtpInput>
+                <button className='verfOtp2' onClick={onOTPVerify}>
+                  {loading && (<CgSpinner size={20} className='spinner'/>)}
+                  <span>Verify OTP</span>
+                </button>
+              </>) : (
+                <>
+                  <div className='logo'>
+                    <BsTelephoneFill size={30} />
+                  </div>
+                  <label className='phone' htmlFor="" >
+                    Verify your phone no.
+                  </label>
+                  <PhoneInput className='phninp' country={"in"} value={ph} onChange={setPh} />
+                  <button className='verfOtp' onClick={onSignup}>
+                    {loading && (<CgSpinner size={20} />)}
+                    <span>Send code via SMS</span>
+                  </button>
+                </>
+              )}
+
+          </div>
+      )}
+      
     </div>
+  </section>
   );
 }
 
-export default App;
+export default App
---
+++ src/firebase.config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAUmcXT5TVHG1FFVjAUR0Of1i-OCoFMxNg",
+    authDomain: "phone-auth-54d37.firebaseapp.com",
+    projectId: "phone-auth-54d37",
+    storageBucket: "phone-auth-54d37.appspot.com",
+    messagingSenderId: "81152055268",
+    appId: "1:81152055268:web:f2980428586e2cd13999f1",
+    measurementId: "G-229RBBBPP7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth=getAuth(app)
